a2b235a4d53dcc8b6c378a69048a8905d92b1710
Pavel Pashkovich <pashkovich.pavel@gmail.com>
2022-08-05 00:11:23 +0300

    complete Task2

---
+++ Task2.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace src;
+
+class Task2
+{
+    public function main(string $date): int
+    {
+        if (!$this->validDate($date)) {
+            throw new \InvalidArgumentException("main function only accepts 'DD.MM.YYYY' date format. Input was: " . $date);
+        }
+        $now = date_create(date('d.m.Y'));
+        $birthday = date_create($date);
+        $diff = date_diff($birthday, $now);
+        if ($birthday > $now) {
+            return $diff->days;
+        } else {
+            return '-' . $diff->days;
+        }
+    }
+
+    public function validDate(string $date): bool
+    {
+        $dateObj = date_create_from_format('d.m.Y', $date);
+        return $dateObj && $dateObj->format('d.m.Y') === $date;
+    }
+}
