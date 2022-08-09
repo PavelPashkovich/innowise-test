d4828b07404e3a467fc397c30fbc26150e022c94
Pavel Pashkovich <pashkovich.pavel@gmail.com>
2022-08-09 19:02:10 +0300

    complete Task10

---
+++ Task10.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace src;
+
+class Task10
+{
+    public function main(int $n): array
+    {
+        if ($n < 1) {
+            throw new \InvalidArgumentException('function accepts positive numbers only. Input was: ' . $n);
+        }
+
+        $result = [$n];
+        while ($n > 1) {
+            $n % 2 === 0 ? $n = $n / 2 : $n = $n * 3 + 1;
+            $result[] = $n;
+        }
+        return $result;
+    }
+}
