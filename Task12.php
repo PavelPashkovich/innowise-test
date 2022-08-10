9bf5405e31c224d0dc0467563337d7be4f5d23e8
Pavel Pashkovich <pashkovich.pavel@gmail.com>
2022-08-10 11:51:43 +0300

    complete Task12

---
+++ Task12.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace src;
+
+class Task12
+{
+    public float $a;
+    public float $b;
+    public float $result = 0;
+
+    public function __construct(float $a, float $b)
+    {
+        $this->a = $a;
+        $this->b = $b;
+    }
+
+    public function add(): object
+    {
+        $this->result = $this->a + $this->b;
+        return $this;
+    }
+
+    public function subtract(): object
+    {
+        $this->result = $this->a - $this->b;
+        return $this;
+    }
+
+    public function multiply(): object
+    {
+        $this->result = $this->a * $this->b;
+        return $this;
+    }
+
+    public function divide(): object
+    {
+        $this->result = $this->a / $this->b;
+        return $this;
+    }
+
+    public function addX(float $x): object
+    {
+        $this->result += $x;
+        return $this;
+    }
+
+    public function subtractX(float $x): object
+    {
+        $this->result -= $x;
+        return $this;
+    }
+
+    public function multiplyBy(float $x): object
+    {
+        $this->result *= $x;
+        return $this;
+    }
+
+    public function divideBy(float $x): object
+    {
+        $this->result /= $x;
+        return $this;
+    }
+
+    public function getResult(): float
+    {
+        return $this->result;
+    }
+}
