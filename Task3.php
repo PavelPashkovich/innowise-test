79ed81be04f4fb132ca5a51765d737f6874958ce
Pavel Pashkovich <pashkovich.pavel@gmail.com>
2022-08-11 17:07:04 +0300

    fix Task3

--- Task3.php
+++ Task3.php
@@ -6,6 +6,10 @@ class Task3
 {
     public function main(int $number): int
     {
+        if ($number <= 1) {
+            throw new \InvalidArgumentException('function accepts numbers > 1 only. Input was: ' . $number);
+        }
+
         while (strlen($number) > 1) {
             $array = str_split($number);
             $number = array_sum($array);
