6b8b977e53c8ffaa517d6b3eb5df525452637402
Pavel Pashkovich <pashkovich.pavel@gmail.com>
2022-08-11 16:26:46 +0300

    fix Task4

--- Task4.php
+++ Task4.php
@@ -7,6 +7,7 @@ class Task4
     public function main(string $input): string
     {
         $wordsToUpper = ucwords($input, '-_ ');
+
         return str_replace(['-', '_', ' '], '', $wordsToUpper);
     }
 }
