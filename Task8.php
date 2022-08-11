2617e5c30b151e3d79d815af16a48eb8e03f2b63
Pavel Pashkovich <pashkovich.pavel@gmail.com>
2022-08-11 23:57:03 +0300

    fix Task8

--- Task8.php
+++ Task8.php
@@ -7,8 +7,8 @@ class Task8
     public function main(string $json): void
     {
         $data = json_decode($json, true);
-        if (!(json_last_error() === JSON_ERROR_NONE)) {
-            throw new \InvalidArgumentException('function accepts valid json only');
+        if (!(is_array($data) && json_last_error() === JSON_ERROR_NONE)) {
+            throw new \InvalidArgumentException('function should accept valid json only');
         }
         $result = $this->toUnnestedArray($data);
         foreach ($result as $key => $value) {
@@ -18,6 +18,12 @@ class Task8
 
     public function toUnnestedArray(array $array): array
     {
+        if (!is_array($array)) {
+            throw new \InvalidArgumentException('function should accept array only');
+        }
+        if (empty($array)) {
+            throw new \InvalidArgumentException('function accepts empty array as argument');
+        }
         $result = [];
         foreach ($array as $key => $value) {
             if (!is_array($value)) {
