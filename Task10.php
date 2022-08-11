867ac1ad0c6b2d1657db516ea98ce15ab5d31477
Pavel Pashkovich <pashkovich.pavel@gmail.com>
2022-08-11 16:38:55 +0300

    fix Task10

--- Task10.php
+++ Task10.php
@@ -9,7 +9,6 @@ class Task10
         if ($n < 1) {
             throw new \InvalidArgumentException('function accepts positive numbers only. Input was: ' . $n);
         }
-
         $result = [$n];
         while ($n > 1) {
             $n % 2 === 0 ? $n = $n / 2 : $n = $n * 3 + 1;
